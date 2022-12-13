a63426e6d296fd8a4f18cc465ef258938722874f
khanhsvan <118794637+khanhsvan@users.noreply.github.com>
2022-12-13 14:55:28 +0700

    Update la_van_khanh_test2.sql

--- la_van_khanh_test2.sql
+++ la_van_khanh_test2.sql
@@ -26,7 +26,7 @@ create table HangXuat(
 insert into VatTu values ('B01','But Chi','Dong',1000),('B02','But Bi','Dong',1000)
 insert into HDBan values ('KH01','10-10-2022','Nguyen Van A'),('KH02','11-11-2022','Nguyen Van B')
 insert into HangXuat values ('KH01','B01','5000',200),('KH01','B01','5000',250),('KH02','B01','5000',500),('KH02','B02','5000',200)
-------------------------------------------------------------
+------------------------------------------------------------2
 declare @Tong as table(
 	MaHD char(9),
 	TongTien int	
@@ -37,7 +37,7 @@ insert into @Tong
 	select MaHD, MAX(DonGia*@TInhTong) from HangXuat 
 ------------------------------------------------------------
 
------------------------------------------------------------
+-----------------------------------------------------------4
 Create function lst (@MaHD char(9))
 returns int 
 as
@@ -45,14 +45,12 @@ begin
 	return(select HangXuat.MaHD,HDBan.NgayXuat,MaVT,DonGia,SLBan, CONVERT(varchar, datename(HDBan.NgayXuat)) from HangXuat join HDBan on HDBan.MaHD= HangXuat.MaHD where @MaHD = HangXuat.MaHD)
 end 
 go
---------------------------------------------------------------
-CREATE PROCEDURE spNhanvien
-	@MaHD VARCHAR(50) OUT
+--------------------------------------------------------------3
+create function tlist (@mahd nvarchar(10))
+returns table
+as
+	return (select a.MaHD, b.NgayXuat, c.MaVT, a.DonGia, a.SLBan, DATENAME(WEEKDAY,b.NgayXuat) as 'NgayThu'
+			from HangXuat a, HDBan b, VatTu c
+			where a.MaHD = b.MaHD and a.MaVT = c.MaVT and b.MaHD = @mahd)
 
-AS
-BEGIN
-DECLARE @thang from inserted;
-declare @nam from inserted;
- select sum(DonGia*SLBan) as tongtienVT from HangXuat
- where convert(NgayXuat,)
-END;
+select * from tlist('KH01')
